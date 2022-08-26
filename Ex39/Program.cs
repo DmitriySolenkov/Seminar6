c170d23b04190256987df953da86dfbde2e9e82c
KreoX <soldimksol@gmail.com>
2022-08-26 18:14:11 +0300

    Ex39

---
+++ Ex39/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter array's length");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+for (int i = 0; i < length; i++)
+{
+    array[i] = new Random().Next(-10, 11);
+}
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int buffer;
+for (int k = 0; k < length / 2; k++)
+{
+    buffer = array[k];
+    array[k] = array[length - k - 1];
+    array[length - k - 1] = buffer;
+}
+Console.WriteLine($"[{String.Join(", ", array)}]");

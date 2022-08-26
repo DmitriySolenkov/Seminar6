62e3a399dffe8a13f99ec7424227379c6badf70b
KreoX <soldimksol@gmail.com>
2022-08-26 19:15:32 +0300

    Ex44

---
+++ Ex44/Program.cs
@@ -0,0 +1,15 @@
+﻿int num=0;
+while (num <= 2)
+{
+    Console.WriteLine("Введите число более 2:");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+int [] array= new int [num];
+array[0]=0;
+array[1]=array[2]=1;
+for (int i = 2; i < num; i++)
+{
+    array[i]=array[i-1]+array[i-2];
+}
+Console.WriteLine($"{String.Join(" ", array)}");
+
